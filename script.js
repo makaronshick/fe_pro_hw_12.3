1ac381ddae80ef28ccf2a7fcacc1ef689ac36b56
makaronshick <makaronshick@ukr.net>
2024-08-13 17:18:43 +0300

    main logic added

---
+++ script.js
@@ -0,0 +1,49 @@
+"use strict";
+
+function createToDoList() {
+  const listElement = document.createElement("ul");
+  const inputElement = document.createElement("input");
+  inputElement.setAttribute("type", "text");
+
+  const addBtnElement = createButton("Add task", ["add_btn"]);
+
+  addBtnElement.addEventListener("click", () => {
+    if (inputElement.value) {
+      const itemElement = document.createElement("li");
+      itemElement.textContent = inputElement.value;
+      const delBtnElement = createButton("Delete task", ["del_btn"]);
+      listElement.append(itemElement, delBtnElement);
+      inputElement.value = "";
+    } else {
+      alert("Task can't be empty");
+    }
+  });
+
+  listElement.addEventListener("click", (event) => {
+    if (event.target.className !== "del_btn") {
+      return;
+    }
+
+    event.target.previousSibling.remove();
+    event.target.remove();
+  });
+
+  document.body.append(listElement);
+  document.body.append(inputElement);
+  document.body.append(addBtnElement);
+}
+
+function createButton(buttonText, classes = null) {
+  const button = document.createElement("button");
+  button.textContent = buttonText;
+  
+  if (classes !== null) {
+    classes.forEach(element => {
+      button.classList.add(element);
+    });
+  }
+
+  return button;
+}
+
+createToDoList();
